63106f8fe5bd238e9c05c652528737991f97bbe8
Richard Wang <rlw324@cornell.edu>
2020-05-22 18:24:33 -0400

    Initial Commit

---
+++ flashcards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  flashcards
+//
+//  Created by Richard Wang on 5/22/20.
+//  Copyright © 2020 Richard Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
